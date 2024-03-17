4d6619992e7d65cc8cdf0914f68e527509b9020b
Simpauldave <160127854+Simpauldave@users.noreply.github.com>
2024-03-17 22:07:58 +0800

    Add files via upload

---
+++ SIMPRON_OOP_Night_Act2.3/src/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter initial text: ");
+        String initialText = scanner.nextLine();
+        System.out.print("Choose editor type (1 for Print, 2 for Advanced): ");
+        int editorType = scanner.nextInt();
+        TextEditor editor;
+
+        if (editorType == 2) {
+            editor = new AdvancedTextEditor(initialText);
+        } else {
+            editor = new PrintTextEditor(initialText);
+        }
+
+        Tester.test(editor);
+    }
+}
+
+    
+    
+
---
+++ SIMPRON_OOP_Night_Act2.3/src/main/PrintTextEditor.java
@@ -0,0 +1,13 @@
+package main;
+
+public class PrintTextEditor extends TextEditor {
+
+  public PrintTextEditor(String text) {
+    super(text);
+  }
+
+  public void print() {
+    System.out.println(getText());
+  }
+}
+
